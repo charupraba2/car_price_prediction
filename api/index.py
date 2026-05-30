cb21f41d77f0bc7306c2aed24cd3fb3ebd0a7f0e
charupraba2 <charupraba2@gmail.com>
2026-05-30 20:48:51 +0530

    feat: add API entry point and update container healthchecks to use Python urllib

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import sys
+import os
+
+# Add the parent directory of this file to the python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
